708b7d0f84cb7c1dc4286c62c95a75f9612f2180
Guy Steele-Perkins <guysteeleperkins.com>
2025-01-06 11:01:29 +0000

    Added Javascript functionality to past and upcoming events page buttons

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const upcomingEvents = document.getElementById('upcoming-events');
+    const pastEvents = document.getElementById('past-events');
+    const upcomingBtn = document.getElementById('upcoming-btn');
+    const pastBtn = document.getElementById('past-btn');
+
+    function toggleEventsSelection(isUpcoming) {
+
+        if (isUpcoming) {
+            upcomingEvents.style.display = 'block';
+            pastEvents.style.display = 'none';
+            upcomingBtn.classList.add('active');
+            pastBtn.classList.remove('active');
+        } else {
+            upcomingEvents.style.display = 'none';
+            pastEvents.style.display = 'block';
+            upcomingBtn.classList.remove('active');
+            pastBtn.classList.add('active');
+        }
+    }
+
+    upcomingBtn.addEventListener('click', function () {
+        toggleEventsSelection(true);
+    });
+
+    pastBtn.addEventListener('click', function () {
+        toggleEventsSelection(false);
+    });
+
+});
