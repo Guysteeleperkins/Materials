e6b850a84881edcb695ed51ce9ba44e511effa02
Guy Steele-Perkins <guysteeleperkins.com>
2025-01-06 12:26:04 +0000

    Edited database in settings for a test

--- Materials/settings.py
+++ Materials/settings.py
@@ -90,11 +90,18 @@ cloudinary.config(secure=True,)
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL'),
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
